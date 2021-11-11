1307e49f5bb3df78fc940484b04f8166ebe54755
JAFB321 <jafb321@gmail.com>
2021-11-11 01:43:00 -0700

    feat: add try catch to api request

--- src/app.js
+++ src/app.js
@@ -10,126 +10,147 @@ app.use('/', express.static(__dirname + '/public'));
 // Router
 app.get('/api/salon', async (req, res) => {
 
-    const salon = DBsalon();
-    const items = await salon.find({}, 'nombre');
-    
-    res.json(items);
+    try {
+        const salon = DBsalon();
+        const items = await salon.find({}, 'nombre');
+        
+        res.json(items);
+    } catch (error) {
+        
+    }
+
 }); 
 
 
 app.post('/api/salon', async (req, res) => {
-    const { nombre } = req.body;
+    try {
+        const { nombre } = req.body;
 
-    const data  = {
-        nombre,
-        alumnos: []
-    }
-    
-    const salon = DBsalon();
-    const item = await salon.create(data);
-    
-    res.json(item);
+        const data  = {
+            nombre,
+            alumnos: []
+        }
+        
+        const salon = DBsalon();
+        const item = await salon.create(data);
+        
+        res.json(item);
+    } catch (error) {}
 }); 
 
 
 app.post('/api/salon/:id/alumno', async (req, res) => {
-    const {id} = req.params;
-    const { expediente, nombre } = req.body;
+    try {
+        const {id} = req.params;
+        const { expediente, nombre } = req.body;
 
-    const data  = {
-        nombre,
-        expediente,
-        lecturas: []
-    }
+        const data  = {
+            nombre,
+            expediente,
+            lecturas: []
+        }
+        
+        const salon = DBsalon();
+        const item = await salon.findById(id);
+
+        if(item.alumnos === undefined){
+            item.alumnos = [];
+        }
+
+        item.alumnos.push(data);
+        const done = await item.save();
+        
+        res.json({
+            item: done
+        });
+    } catch (error) {}
     
-    const salon = DBsalon();
-    const item = await salon.findById(id);
-
-    if(item.alumnos === undefined){
-        item.alumnos = [];
-    }
-
-    item.alumnos.push(data);
-    const done = await item.save();
-    
-    res.json({
-        item: done
-    });
 }); 
 
 app.get('/api/salon/:id', async (req, res) => {
-    const {id} = req.params;
-    
-    const salon = DBsalon();
-    const item = await salon.findById(id);
-     
-    item.alumnos.forEach(al => {
-        al.lecturas = [];
-    });
+    try {
+        const {id} = req.params;
+        
+        const salon = DBsalon();
+        const item = await salon.findById(id);
+        
+        item.alumnos.forEach(al => {
+            al.lecturas = [];
+        });
 
-    res.json({
-        item: item
-    });
+        res.json({
+            item: item
+        });
+    } catch (error) {
+        
+    }
+    
 }); 
 
 
 app.post('/api/alumno/:id/lectura', async (req, res) => {
-    const { id } = req.params;
-    const { temp, ox, bpm } = req.body;
-
-
-    const data  = {
-        temp,
-        ox,
-        bpm,
-        timestamp: Date.now()
-    }
+    try {
+        const { id } = req.params;
+        const { temp, ox, bpm } = req.body;
     
-    const salon = DBsalon();
     
-    const items = await salon.find({});
-
-    // const item = items.find(({_doc: item}) => item.alumnos.includes(({_doc: al}) => {
-    //     return al.expediente === id;
-    // }));
-    
-    // const item3 = items.forEach(({_doc: item}) => console.log(item));
-
-    const item = items.find(({_doc: item}) => {
-        const al = item.alumnos.find(({_doc: al}) => al.expediente === id);
-
-        return al !== undefined;
-    });
-
-
-
-    if(item){
-        item.alumnos.forEach((al) => {
-            if(al.expediente === id){
-                if(al.lecturas === undefined){
-                    al.lecturas = [];
-                }
-                
-                al.ultimaLectura = data;s
-                al.lecturas.push(data);
-            }
-        });
-        const done = await item.save();
+        const data  = {
+            temp,
+            ox,
+            bpm,
+            timestamp: Date.now()
+        }
         
-        res.json({
-            error: !done
+        const salon = DBsalon();
+        
+        const items = await salon.find({});
+    
+        // const item = items.find(({_doc: item}) => item.alumnos.includes(({_doc: al}) => {
+        //     return al.expediente === id;
+        // }));
+        
+        // const item3 = items.forEach(({_doc: item}) => console.log(item));
+    
+        const item = items.find(({_doc: item}) => {
+            const al = item.alumnos.find(({_doc: al}) => al.expediente === id);
+    
+            return al !== undefined;
         });
+    
+    
+    
+        if(item){
+            item.alumnos.forEach((al) => {
+                if(al.expediente === id){
+                    if(al.lecturas === undefined){
+                        al.lecturas = [];
+                    }
+                    
+                    al.ultimaLectura = data;s
+                    al.lecturas.push(data);
+                }
+            });
+            const done = await item.save();
+            
+            res.json({
+                error: !done
+            });
+        }
+        else{
+            res.json({
+                error: true
+            });
+        }
+    } catch (error) {
+        
     }
-    else{
-        res.json({
-            error: true
-        });
-    }
+    
 
 }); 
 
 app.get('/api/alumno/:id', async (req, res) => {
-    const { id } = req.params;
+    try {
+        const { id } = req.params;
     
     const salon = DBsalon();
     const items = await salon.find({});
@@ -157,6 +178,10 @@ app.get('/api/alumno/:id', async (req, res) => {
             error: true
         });
     }
+    } catch (error) {
+        
+    }
+    
 
 }); 
 
